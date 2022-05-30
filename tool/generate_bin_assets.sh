1d072e9c5390b86d1d31c7b4bbebdf546f3704ea
KonstantinKai <kosyak46@gmail.com>
2022-05-31 00:09:50 +0300

    fix postbootstrap script

--- tool/generate_bin_assets.sh
+++ tool/generate_bin_assets.sh
@@ -5,19 +5,25 @@ SIZES=('1m' '20m')
 
 EXECUTABLE=`which mkfile`
 
+IS_TRUNCATE=false
+
 if [ ! -f "$EXECUTABLE" ];then
   EXECUTABLE=`which truncate`
   if [ ! -f "$EXECUTABLE" ];then
     echo "Cannot generate bin files on this platform"
     exit 1
     else
-    EXECUTABLE="$EXECUTABLE -t"
+    IS_TRUNCATE=true
   fi
 fi
 
 for i in "${!FILES[@]}"; do
   if [ ! -f "${FILES[$i]}" ];then
-    $EXECUTABLE ${SIZES[$i]} ${FILES[$i]}
+    if [[ $IS_TRUNCATE ]];then
+      $EXECUTABLE --size=${SIZES[$i]} ${FILES[$i]}
+      else
+      $EXECUTABLE ${SIZES[$i]} ${FILES[$i]}
+    fi
     echo "${FILES[$i]} created"
   fi
 done
--- uploadcare_client/lib/src/pubspec.dart
+++ uploadcare_client/lib/src/pubspec.dart
@@ -3,21 +3,21 @@
 // ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
 
 class Pubspec {
-  static final buildDate = DateTime.utc(2022, 5, 30, 20, 39, 33);
+  static final buildDate = DateTime.utc(2022, 5, 30, 20, 57, 19);
 
   static const name = 'uploadcare_client';
 
-  static const versionFull = '6.0.1';
+  static const versionFull = '6.1.0';
 
-  static const version = '6.0.1';
+  static const version = '6.1.0';
 
-  static const versionSmall = '6.0';
+  static const versionSmall = '6.1';
 
   static const versionMajor = 6;
 
-  static const versionMinor = 0;
+  static const versionMinor = 1;
 
-  static const versionPatch = 1;
+  static const versionPatch = 0;
 
   static const versionBuild = 0;
 
