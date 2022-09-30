09ae49210e870821180455334427ec91f805fd99
KonstantinKai <kosyak46@gmail.com>
2022-09-30 17:28:17 +0300

    update pubspec

--- uploadcare_client/lib/src/pubspec.dart
+++ uploadcare_client/lib/src/pubspec.dart
@@ -3,21 +3,21 @@
 // ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
 
 class Pubspec {
-  static final buildDate = DateTime.utc(2022, 7, 27, 8, 48, 57);
+  static final buildDate = DateTime.utc(2022, 9, 30, 14, 26, 46);
 
   static const name = 'uploadcare_client';
 
-  static const versionFull = '6.1.1';
+  static const versionFull = '6.2.0';
 
-  static const version = '6.1.1';
+  static const version = '6.2.0';
 
-  static const versionSmall = '6.1';
+  static const versionSmall = '6.2';
 
   static const versionMajor = 6;
 
-  static const versionMinor = 1;
+  static const versionMinor = 2;
 
-  static const versionPatch = 1;
+  static const versionPatch = 0;
 
   static const versionBuild = 0;
 
