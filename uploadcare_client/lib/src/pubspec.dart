9f5977ac52e5c56f772bbd9de82e159f8079dad6
KonstantinKai <kosyak46@gmail.com>
2022-11-11 12:33:08 +0200

    Fixed meta package version conflict, issue#9

--- uploadcare_client/lib/src/pubspec.dart
+++ uploadcare_client/lib/src/pubspec.dart
@@ -3,13 +3,13 @@
 // ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
 
 class Pubspec {
-  static final buildDate = DateTime.utc(2022, 9, 30, 14, 26, 46);
+  static final buildDate = DateTime.utc(2022, 11, 11, 10, 28, 55);
 
   static const name = 'uploadcare_client';
 
-  static const versionFull = '6.2.0';
+  static const versionFull = '6.2.1';
 
-  static const version = '6.2.0';
+  static const version = '6.2.1';
 
   static const versionSmall = '6.2';
 
@@ -17,7 +17,7 @@ class Pubspec {
 
   static const versionMinor = 2;
 
-  static const versionPatch = 0;
+  static const versionPatch = 1;
 
   static const versionBuild = 0;
 
