12f2643bb6af6a7b6bec6970f59cde54f410d740
Konstantin Kai <kosyak46@gmail.com>
2025-03-10 12:42:38 +0200

    chore: pubspec gen

--- uploadcare_client/lib/src/pubspec.dart
+++ uploadcare_client/lib/src/pubspec.dart
@@ -3,19 +3,19 @@
 // ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
 
 class Pubspec {
-  static final buildDate = DateTime.utc(2023, 10, 26, 9, 34, 18);
+  static final buildDate = DateTime.utc(2025, 3, 10, 10, 41, 42);
 
   static const name = 'uploadcare_client';
 
-  static const versionFull = '7.0.0';
+  static const versionFull = '7.1.0';
 
-  static const version = '7.0.0';
+  static const version = '7.1.0';
 
-  static const versionSmall = '7.0';
+  static const versionSmall = '7.1';
 
   static const versionMajor = 7;
 
-  static const versionMinor = 0;
+  static const versionMinor = 1;
 
   static const versionPatch = 0;
 
